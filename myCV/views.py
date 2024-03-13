01f796a76a9f3e3931ecb98fdee526c8d730fcc9
Win11 <tarkankonakci@gmail.com>
2024-03-13 21:41:03 +0300

    feat: create contact page

--- myCV/views.py
+++ myCV/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+def index(request):
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
 
 # Create your views here.
